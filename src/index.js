18f2a5ee96c2ee37d4050cbc13dbe96f82c56466
Zach Williamson <zach.williamson@vml.com>
2016-06-28 09:09:36 -0500

    Prevent debugger from showing initially

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ var mainElement = document.getElementById('main');
 var app = Elm.Main.embed(mainElement);
 
 app.ports.debug.subscribe(function(isVisible) {
+    // This port allows us to hide/show the time travel debugger
     var debugInterface = mainElement.lastChild.lastChild;
     if (debugInterface.style.display === 'none') {
         debugInterface.style.display = 'initial';
@@ -13,3 +14,16 @@ app.ports.debug.subscribe(function(isVisible) {
         debugInterface.style.display = 'none';
     }
 });
+
+
+var hideDebugInterface = setInterval(function() {
+    // This updates the stypes of the time travel debugger so that its hidden
+    // by default and so that the debugger can be scrolled.
+    debugInterface = mainElement.lastChild.lastChild;
+    if (debugInterface != null) {
+        debugInterface.getElementsByTagName('div')[0]
+          .style.position = 'absolute'
+        debugInterface.style.display = 'none';
+        clearInterval(hideDebugInterface);
+    }
+}, 5)
