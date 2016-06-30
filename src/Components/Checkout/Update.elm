9b6d6fb4c023a6cdc5bd5bc06cc454e2fe75e498
Zach Williamson <zach.williamson@vml.com>
2016-06-30 08:28:44 -0500

    Add Components.Checkout.Update

---
+++ src/Components/Checkout/Update.elm
@@ -0,0 +1,23 @@
+module Components.Checkout.Update exposing (..)
+
+-- CORE MODULES
+
+
+-- OUR MODULES
+
+import Components.SelectPayment.Update as SelectPaymentUpdate
+import Components.Checkout.Types exposing (..)
+import Components.Checkout.Model exposing (Model)
+
+-- UPDATE
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    SelectPaymentMsg msg ->
+      let
+        (model', cmds) =
+          SelectPaymentUpdate.update msg { newCard = model.newCard }
+      in
+        { model | newCard = model'.newCard }
+        ! []
