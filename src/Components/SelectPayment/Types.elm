c0b77257f4698763c10bce3222b9fa4318afd0f6
Zach Williamson <zach.williamson@vml.com>
2016-06-30 08:27:16 -0500

    Add SelectPayment.Types

---
+++ src/Components/SelectPayment/Types.elm
@@ -0,0 +1,10 @@
+module Components.SelectPayment.Types exposing (..)
+
+-- OUR MODULES
+
+import Components.CreateCardForm.Types as CreateCardFormTypes
+
+-- TYPES
+
+type Msg
+  = CreateCardFormMsg CreateCardFormTypes.Msg
