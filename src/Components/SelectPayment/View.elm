2135fed1f63c1ec02e11b13b7794503713c9784c
Zach Williamson <zach.williamson@vml.com>
2016-07-02 04:33:43 -0500

    Update SelectPayment view

--- src/Components/SelectPayment/View.elm
+++ src/Components/SelectPayment/View.elm
@@ -16,27 +16,36 @@ import Components.NewCardForm.View as NewCardFormView
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ div [ class "layout layout--huge" ]
-      [ div [ class "layout__item u-1/2" ]
-        [ h4 [ class "u-m0" ] [ text "New Card" ]
-        , hr [ class "u-mb" ] []
-        , App.map NewCardFormMsg (NewCardFormView.view model.newCard)
-        , div [ class "spread u-mt" ]
-          [ label [ class "label" ]
-            [ span [ class "u-pr-" ] [ text "Save Card" ]
-            , input [ type' "checkbox", value "text" ] []
+  let
+    formValidation =
+      div [] (List.map formErrorMessage model.newCard.errorMessages)
+
+    formErrorMessage errMsg =
+      div [ class "u-txt-red" ] [ text errMsg ]
+
+  in
+    div []
+      [ div [ class "layout layout--huge" ]
+        [ div [ class "layout__item u-1/2" ]
+          [ h4 [ class "u-m0" ] [ text "New Card" ]
+          , hr [ class "u-mb" ] []
+          , App.map NewCardFormMsg (NewCardFormView.view model.newCard)
+          , div [ class "spread u-mv" ]
+            [ label [ class "label" ]
+              [ span [ class "u-pr-" ] [ text "Save Card" ]
+              , input [ type' "checkbox", value "text" ] []
+              ]
+            , button [ classList [("btn", not model.newCard.hasError), ("btn btn--disabled", model.newCard.hasError)], disabled model.newCard.hasError ] [ text "Order Now" ]
             ]
-          , button [ class "btn" ] [ text "Order Now" ]
+          , formValidation
           ]
-        ]
-      , div [ class "layout__item u-1/2" ]
-        [ h4 [ class "u-m0" ] [ text "Saved Card" ]
-        , hr [ class "u-mb" ] []
-        , div [ class "spread" ]
-          [ div [] [ text "Card ending in 2456" ]
-          , button [ class "btn" ] [ text "Order Now" ]
+        , div [ class "layout__item u-1/2" ]
+          [ h4 [ class "u-m0" ] [ text "Saved Card" ]
+          , hr [ class "u-mb" ] []
+          , div [ class "spread" ]
+            [ div [] [ text "Card ending in 2456" ]
+            , button [ class "btn" ] [ text "Order Now" ]
+            ]
           ]
         ]
       ]
-    ]
