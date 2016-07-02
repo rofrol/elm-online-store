c552bd6c059db8dd0dbe8ab3db6b550a07bdd781
Zach Williamson <zach.williamson@vml.com>
2016-07-02 04:33:34 -0500

    Update form logic and validation for NewCardForm

--- src/Components/NewCardForm/Update.elm
+++ src/Components/NewCardForm/Update.elm
@@ -1,28 +1,98 @@
 module Components.NewCardForm.Update exposing (update)
 
+-- CORE MODULES
+
+import Update.Extra exposing (andThen)
+
 -- OUR MODULES
 
 import Components.NewCardForm.Types exposing (..)
 import GeneralTypes.NewCard exposing (NewCard)
-import Utils.Format exposing (formatCardNumber, formatCardExpiration, formatCVV)
+import UtilsAndConstants.Format exposing (formatCardNumber, formatCardExpiration, formatCVV)
+import UtilsAndConstants.FormValidators exposing (validateCardNumber, validateName, validateCardExp, validateCardCVV)
+import UtilsAndConstants.Errors exposing (cardNumberErrorMsg, nameErrorMsg, cardExpErrorMsg, cardCVVErrorMsg)
 
 -- UPDATE
 
 update : Msg -> NewCard -> (NewCard, Cmd Msg)
 update msg model =
   case msg of
-    CardNumber newNum ->
-      { model | cardNumber = formatCardNumber newNum}
-      ! []
+    UpdateCardNumber newNum ->
+      { model | cardNumber = formatCardNumber newNum } ! []
+        |> andThen update UpdateHasError
+        |> andThen update (MaybeClearErrorMessage CardNumberError)
 
-    Name newName ->
-      { model | name = newName }
-      ! []
+    UpdateName newName ->
+      { model | name = newName } ! []
+        |> andThen update UpdateHasError
+        |> andThen update (MaybeClearErrorMessage CardNameError)
 
-    ExpirationDate newExp ->
-      { model | exp = formatCardExpiration newExp }
-      ! []
+    UpdateExpirationDate newExp ->
+      { model | exp = formatCardExpiration newExp } ! []
+        |> andThen update UpdateHasError
+        |> andThen update (MaybeClearErrorMessage CardExpError)
 
-    CVV newCVV ->
-      { model | cvv = formatCVV newCVV }
-      ! []
+    UpdateCVV newCVV ->
+      { model | cvv = formatCVV newCVV } ! []
+        |> andThen update UpdateHasError
+        |> andThen update (MaybeClearErrorMessage CardCVVError)
+
+    UpdateHasError ->
+      if validateCardNumber model.cardNumber
+      && validateName model.name
+      && validateCardExp model.exp
+      && validateCardCVV model.cvv
+      then
+        { model | hasError = False } ! []
+      else
+        { model | hasError = True } ! []
+
+    MaybeClearErrorMessage errorType ->
+      let
+        filterOutError : (String -> Bool) -> String -> String -> (NewCard, Cmd Msg)
+        filterOutError validator valueToTest message =
+          if (validator valueToTest) then
+            { model | errorMessages = List.filter
+              (\errMsg -> not (errMsg == message))
+              model.errorMessages
+            } ! []
+          else
+            model ! []
+      in
+        case errorType of
+          CardNumberError ->
+              filterOutError validateCardNumber model.cardNumber cardNumberErrorMsg
+          CardNameError ->
+              filterOutError validateName model.name nameErrorMsg
+          CardExpError ->
+              filterOutError validateCardExp model.exp cardExpErrorMsg
+          CardCVVError ->
+              filterOutError validateCardCVV model.cvv cardCVVErrorMsg
+
+    MaybeSetErrorMessage errorType ->
+      let
+        (errorString, isError) =
+          case errorType of
+            CardNumberError ->
+              ( cardNumberErrorMsg
+              , not (validateCardNumber model.cardNumber)
+              )
+            CardNameError ->
+              ( nameErrorMsg
+              , not (validateName model.name)
+              )
+            CardExpError ->
+              ( cardExpErrorMsg
+              , not (validateCardExp model.exp)
+              )
+            CardCVVError ->
+              ( cardCVVErrorMsg
+              , not (validateCardCVV model.cvv)
+              )
+      in
+        if isError
+        && not (List.any (\errMsg -> errMsg == errorString) model.errorMessages) then
+          { model | errorMessages = errorString :: model.errorMessages}
+          ! []
+        else
+          model ! []
--- src/Components/NewCardForm/View.elm
+++ src/Components/NewCardForm/View.elm
@@ -4,12 +4,12 @@ module Components.NewCardForm.View exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
+import Html.Events exposing (onClick, onInput, onBlur)
 
 -- OUR MODULES
 
 import GeneralTypes.NewCard exposing (NewCard)
-import Components.NewCardForm.Types exposing (Msg(..))
+import Components.NewCardForm.Types exposing (Msg(..), FormError(..))
 
 -- VIEW
 
@@ -23,15 +23,17 @@ view card =
           , div [] [ text "Name on Card" ]
           ]
         , div [ class "field-set__fields" ]
-          [ input [ value card.cardNumber, maxlength 19, onInput CardNumber ] []
-          , input [ onInput Name ] []
+          [ input
+            [ value card.cardNumber, maxlength 19, onInput UpdateCardNumber, onBlur (MaybeSetErrorMessage CardNumberError)
+            ] []
+          , input [ onInput UpdateName, onBlur (MaybeSetErrorMessage CardNameError) ] []
           ]
         ]
       , div [ class "field-row" ]
         [ div [] [ text "Expiration Date" ]
-        , input [ value card.exp, maxlength 5, onInput ExpirationDate ] []
+        , input [ value card.exp, maxlength 5, onInput UpdateExpirationDate, onBlur (MaybeSetErrorMessage CardExpError) ] []
         , div [] [ text "CVV" ]
-        , input [ value card.cvv, maxlength 4, onInput CVV ] []
+        , input [ value card.cvv, maxlength 4, onInput UpdateCVV, onBlur (MaybeSetErrorMessage CardCVVError) ] []
         ]
       ]
     ]
