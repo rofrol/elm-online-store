3edd2de21204c41cd02b8819dfdb986bb845be23
Zach Williamson <zach.williamson@vml.com>
2016-07-04 17:06:32 -0500

    Update update logic in Components.SelectPayment.Update

--- src/Components/SelectPayment/Update.elm
+++ src/Components/SelectPayment/Update.elm
@@ -6,6 +6,7 @@ import Components.SelectPayment.Types exposing (..)
 import Components.SelectPayment.Model exposing (Model)
 import Components.Card.Update as CardUpdate
 import Components.Card.Types as CardTypes
+import Update.Extra exposing (andThen)
 
 -- UPDATE
 
@@ -17,11 +18,37 @@ update msg model =
         (model', cmds) =
           CardUpdate.update msg (model.savedCard, model.newCard)
       in
-        Model (Debug.log "test2" (fst model')) (snd model') ! []
+        Model (fst model') (snd model') ! []
 
-    CheckoutAndSave ->
+    UseAndSaveNewCard ->
       let
-        (model', cmds) =
-          CardUpdate.update CardTypes.SaveCard (model.savedCard, model.newCard)
+        saveCard (model', cmd') =
+          andThen update SaveNewCard ( model', cmd' )
+        useNewCard (model', cmd') =
+          andThen update UseNewCard ( model', cmd' )
       in
-        { model | newCard = snd model' } ! [ Cmd.map CardMsg cmds ]
+        model ! []
+          |> saveCard
+          |> useNewCard
+
+    SaveNewCard ->
+      if model.newCard.shouldSave then
+        let
+          (model', cmds) =
+            CardUpdate.update CardTypes.SaveCard (model.savedCard, model.newCard)
+        in
+          { model | newCard = snd model' } ! [ Cmd.map CardMsg cmds ]
+      else
+        model ! []
+
+    UseNewCard ->
+      let
+        updateNewCard card =
+          { card | shouldUse = True }
+        updateSavedCard card =
+          { card | shouldUse = False }
+      in
+        { model
+          | newCard = updateNewCard model.newCard
+          , savedCard = updateSavedCard model.savedCard
+        } ! []
