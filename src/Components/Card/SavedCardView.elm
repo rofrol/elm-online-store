3daea798f1fb6c37fd6630a5a6c7ee82f5daf4d6
Zach Williamson <zach.williamson@vml.com>
2016-07-04 17:05:54 -0500

    Update Components.Card.SavedCardView

--- src/Components/Card/SavedCardView.elm
+++ src/Components/Card/SavedCardView.elm
@@ -4,15 +4,16 @@ module Components.Card.SavedCardView exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 import String
 
 -- OUR MODULES
 
-import Components.Card.Types exposing (SavedCard)
+import Components.Card.Types exposing (..)
 
 -- VIEW
 
-view : SavedCard -> Html a
+view : SavedCard -> Html Msg
 view savedCard =
   let
     last4 =
@@ -24,7 +25,7 @@ view savedCard =
       else
         div [ class "spread" ]
           [ div [] [ text ("Card ending in " ++ last4) ]
-          , button [ class "btn" ] [ text "Order Now" ]
+          , a [ onClick UseSavedCard, class "btn", href "/#/checkout/summary" ] [ text "Order Now" ]
           ]
 
     noSavedCards =
