fd8ebbd65a67958d9794daef3f5b759a01072d8f
Zach Williamson <zach.williamson@vml.com>
2016-07-04 12:54:11 -0500

    Add CheckoutAndSave case to update in Components.SelectPayment.Update

--- src/Components/SelectPayment/Update.elm
+++ src/Components/SelectPayment/Update.elm
@@ -5,15 +5,23 @@ module Components.SelectPayment.Update exposing (..)
 import Components.SelectPayment.Types exposing (..)
 import Components.SelectPayment.Model exposing (Model)
 import Components.Card.Update as CardUpdate
+import Components.Card.Types as CardTypes
 
 -- UPDATE
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    NewCardFormMsg msg ->
+    CardMsg msg ->
       let
         (model', cmds) =
           CardUpdate.update msg (model.savedCard, model.newCard)
       in
-        { model | newCard = snd model' } ! []
+        Model (Debug.log "test2" (fst model')) (snd model') ! []
+
+    CheckoutAndSave ->
+      let
+        (model', cmds) =
+          CardUpdate.update CardTypes.SaveCard (model.savedCard, model.newCard)
+      in
+        { model | newCard = snd model' } ! [ Cmd.map CardMsg cmds ]
