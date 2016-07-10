1d403e5303e4c8f1fc71719c67405dc34a81ce83
Zach Williamson <zach.williamson@vml.com>
2016-07-10 03:10:01 -0500

    Update Msg type in Pages.Checkout.Types

--- src/Pages/Checkout/Types.elm
+++ src/Pages/Checkout/Types.elm
@@ -3,8 +3,10 @@ module Pages.Checkout.Types exposing (Msg(..))
 -- OUR MODULES
 
 import Pages.Checkout.SelectPayment.Types as SelectPaymentTypes
+import Pages.Checkout.OrderSummary.Types as OrderSummaryTypes
 
 -- TYPES
 
 type Msg
   = SelectPaymentMsg SelectPaymentTypes.Msg
+  | OrderSummaryMsg OrderSummaryTypes.Msg
