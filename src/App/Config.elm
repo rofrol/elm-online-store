edd3108da93cac7870ec6cdc12884b48a42847c2
Zach Williamson <zach.williamson@vml.com>
2016-07-14 13:10:17 -0500

    Add headers to App.Config

--- src/App/Config.elm
+++ src/App/Config.elm
@@ -2,6 +2,13 @@ module App.Config exposing (..)
 
 host : String
 host = "http://localhost:8000"
+-- host = "http://zachwilliamson.me:8000"
 
 baseUrl : String
 baseUrl = "/#/"
+-- baseUrl = "/apps/elm-online-store-example/#/"
+
+headers : List ( String, String )
+headers =
+  [ ( "Content-Type", "application/json" )
+  ]
